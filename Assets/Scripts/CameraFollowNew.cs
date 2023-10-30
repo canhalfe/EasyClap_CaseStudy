1388786fe811663e385d7a1c7598fd5f0aa39a10
Can Halfe <canhalfeogullari@gmail.com>
2023-10-30 21:04:59 +0300

    Initial commit

---
+++ Assets/Scripts/CameraFollowNew.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollowNew : MonoBehaviour
+{
+    public Transform PlayerTransform;
+    private Vector3 _cameraOffset;
+
+    [Range(0.01f, 5.0f)]
+    public float SmootFactor = 0.5f;
+
+    public bool LookatPlayer = false;
+    void Start()
+    {
+        _cameraOffset = transform.position - PlayerTransform.position;
+    }
+
+    void FixedUpdate()
+    {
+        //Vector3 newPos = PlayerTransform.position + _cameraOffset;
+
+        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, PlayerTransform.position.z + _cameraOffset.z);
+
+        transform.position = Vector3.Slerp(transform.position, newPos, SmootFactor * Time.fixedDeltaTime);
+
+        if (LookatPlayer)
+        {
+            transform.LookAt(PlayerTransform);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerHorizontalMoving.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHorizontalMoving : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed;
+    [SerializeField] float forwardSpeed;
+    [SerializeField] float horizontalLimit;
+
+    private float mZCoord;
+    private Vector3 mOffset;
+
+    private void Start()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * forwardSpeed * Time.fixedDeltaTime); // forward
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            mZCoord = Camera.main.WorldToScreenPoint(gameObject.transform.position).z;
+
+            mOffset = gameObject.transform.position - GetMouseAsWorldPoint();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 endPos = new Vector3((GetMouseAsWorldPoint().x + mOffset.x), transform.position.y, transform.position.z);
+
+            transform.position = Vector3.Lerp(transform.position, endPos, speed * Time.fixedDeltaTime);
+
+            transform.position = 
+                new Vector3(Mathf.Clamp(transform.position.x, -horizontalLimit, horizontalLimit), transform.position.y, transform.position.z); 
+            // horizontal limit 
+        }
+    }
+
+    private Vector3 GetMouseAsWorldPoint()
+    {
+        Vector3 mousePoint = Input.mousePosition;
+        mousePoint.z = mZCoord;
+
+        // Convert it to world points
+        return Camera.main.ScreenToWorldPoint(mousePoint);
+    }
+}
